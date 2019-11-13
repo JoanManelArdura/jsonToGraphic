8fc8dbba04d56c7dfb23ece323c35fbd14c9c2fd
reki <reki667@hotmail.com>
2019-11-13 18:20:11 +0100

    començant a bastir el programa, afegeixo clase base

---
+++ src/main/java/com/test/JsonToGraphicApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonToGraphicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonToGraphicApplication.class, args);
+	}
+
+}
